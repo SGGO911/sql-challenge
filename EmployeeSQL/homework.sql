ded6d24c4cf793b95bc3d463a979424380bd2671
Sara Patel <gallegosara@hotmail.com>
2021-02-27 19:15:19 -0500

    adding queries

--- EmployeeSQL/homework.sql
+++ EmployeeSQL/homework.sql
@@ -106,6 +106,16 @@ WHERE first_name = 'Hercules'
 AND last_name LIKE  'B%'
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT d.dept_no, d.dept_name
+FROM departments d
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de 
+ON e.emp_no = de.emp_no
+JOIN departments d 
+ON de.dept_no = d.dept_no
+WHERE d.dept_no = 'd007' ;
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
